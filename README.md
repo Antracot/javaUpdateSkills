dc6d14e66536ef15266f7998e496eb257abbfb86
Artem Korchevyi <akorchevyi@akorchevyi-mm.local>
2020-02-13 14:45:03 +0200

    added first task

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # javaUpdateSkills
 prj just for improving my java skills
+
+Tasks form resource 
+https://www.w3resource.com/java-exercises/datatypes/index.php
---
+++ src/main/java/getMinNumber.java
@@ -0,0 +1,17 @@
+public class getMinNumber {
+    public static int min( int a, int b, int c){
+        if (a < b && a < c) {
+            return a;
+        } else if (b < a && b < c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(min(10, 11, 9));
+    }
+
+}
